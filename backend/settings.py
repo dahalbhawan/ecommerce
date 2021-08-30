494c6966a6f97dc9485ee0a6aa8aab4470e37735
Bhawan Mani Dahal <dahalbhawan@gmail.com>
2021-08-30 14:20:32 +1000

    changed middleware positions

--- backend/settings.py
+++ backend/settings.py
@@ -81,10 +81,10 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
